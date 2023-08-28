eb482b62805de13f608ae6b9b1dfd2416550a555
janildoss <janildoucsalinfo@gmail.com>
2023-08-28 15:47:11 -0300

    Projeto criado

---
+++ src/main/java/com/api/prjPedido_BackEnd_Spring/PrjPedidoBackEndSpringApplication.java
@@ -0,0 +1,13 @@
+package com.api.prjPedido_BackEnd_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjPedidoBackEndSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrjPedidoBackEndSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/prjPedido_BackEnd_Spring/PrjPedidoBackEndSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.prjPedido_BackEnd_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjPedidoBackEndSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
