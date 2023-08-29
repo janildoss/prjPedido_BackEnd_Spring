991637da8594a0b728d95f360793db65e937ad10
janildoss <prjPedido_BackEnd_Spring>
2023-08-29 00:37:05 -0300

    Testando a classe de dominio Categoria,

--- src/main/java/com/api/prjPedido_BackEnd_Spring/resouces/CategoriaResource.java
+++ src/main/java/com/api/prjPedido_BackEnd_Spring/resouces/CategoriaResource.java
@@ -1,14 +1,24 @@
 package com.api.prjPedido_BackEnd_Spring.resouces;
 
+import com.api.prjPedido_BackEnd_Spring.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
+
     @RequestMapping(method= RequestMethod.GET )
-    public String listar(){
-        return "Rest esta funcionando";
+    public List<Categoria> listar(){
+        Categoria cat1 = new Categoria(1, "Informatica");
+        Categoria cat2 = new Categoria(2, "Escritorio");
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+       return lista ;
     }
 }
