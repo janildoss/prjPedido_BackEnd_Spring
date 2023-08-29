14ce946e922b35e5e88d8bf81a419c3d068ad599
janildoss <prjPedido_BackEnd_Spring>
2023-08-29 15:41:10 -0300

    Banco de dados H2 e criação automatica da tabela Categoria a partir da classe de dominio

--- src/main/java/com/api/prjPedido_BackEnd_Spring/domain/Categoria.java
+++ src/main/java/com/api/prjPedido_BackEnd_Spring/domain/Categoria.java
@@ -3,8 +3,17 @@ package com.api.prjPedido_BackEnd_Spring.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
-public class Categoria implements Serializable {
-    private static final Long serialVersionUID = 1L;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Categoria implements Serializable{ 	
+	private static final long serialVersionUID = 1L;
+	
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Integer id;
     private String nome;
 
