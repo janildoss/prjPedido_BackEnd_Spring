666ad3e31cc39d5ea902a46a70eec63e6ad4a4ce
janildoss <prjPedido_BackEnd_Spring>
2023-08-30 14:40:16 -0300

    Produtos x Categorias e associaçoes muitos para muitos

--- src/main/java/com/api/prjPedido_BackEnd_Spring/PrjPedidoBackEndSpringApplication.java
+++ src/main/java/com/api/prjPedido_BackEnd_Spring/PrjPedidoBackEndSpringApplication.java
@@ -1,13 +1,14 @@
 package com.api.prjPedido_BackEnd_Spring;
 
 import com.api.prjPedido_BackEnd_Spring.domain.Categoria;
+import com.api.prjPedido_BackEnd_Spring.domain.Produto;
 import com.api.prjPedido_BackEnd_Spring.repositories.CategoriaRepository;
+import com.api.prjPedido_BackEnd_Spring.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
@@ -18,13 +19,28 @@ import java.util.Arrays;
 public class PrjPedidoBackEndSpringApplication implements CommandLineRunner {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	public static void main(String[] args) {
-		SpringApplication.run(PrjPedidoBackEndSpringApplication.class, args);	}
+	@Autowired
+	private ProdutoRepository  produtoRepository;
+	public static void main(String[] args) {SpringApplication.run(PrjPedidoBackEndSpringApplication.class, args);	}
 	@Override
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria(null,"Informatica");
 		Categoria cat2 = new Categoria(null, "Escritorio");
+
+
+		Produto p1 = new Produto(null, "Computador",2000.00 );
+		Produto p2 = new Produto(null, "Impressora",800.00 );
+		Produto p3 = new Produto(null, "Mouse",80.00 );
+		//categoria conhece seus produtos
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList( p2));
+		//produtos conhece suas categorias
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		produtoRepository .saveAll(Arrays.asList(p1, p2, p3));
 
 	}
 }
--- src/main/java/com/api/prjPedido_BackEnd_Spring/domain/Categoria.java
+++ src/main/java/com/api/prjPedido_BackEnd_Spring/domain/Categoria.java
@@ -1,27 +1,32 @@
 package com.api.prjPedido_BackEnd_Spring.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Categoria implements Serializable{ 	
 	private static final long serialVersionUID = 1L;
-	
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Integer id;
     private String nome;
+
+    //Associações  categoria x produto *.*
+    @ManyToMany(mappedBy="categorias")
+    private List<Produto> produtos = new ArrayList<>();
+
+    //Construtores
     public Categoria(){
     }
     public Categoria(Integer id, String nome) {
         this.id = id;
         this.nome = nome;
     }
+    //Geters x Seters
     public Integer getId() {
         return id;
     }
@@ -33,6 +38,13 @@ public class Categoria implements Serializable{
     }
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+    public void setProdutos(List<Produto> produtos) {
+        this.produtos = produtos;
     }
     @Override
     public boolean equals(Object o) {
@@ -47,5 +59,6 @@ public class Categoria implements Serializable{
     public int hashCode() {
         return Objects.hash(getId());
     }
+
 
 }
